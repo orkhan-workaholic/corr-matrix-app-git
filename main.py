7c83aa3e3f98dc6b69270fbb54fc9cbf59c1b03b
orkhan-workaholic <105862853+orkhan-workaholic@users.noreply.github.com>
2022-05-19 17:32:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.sidebar.header('1. Upload File')
 
 
 
-uploaded_file = st.sidebar.file_uploader(label="Upload your CSV or Excel file. Make sure your dataset doesn't have multilevel columns",
+uploaded_file = st.sidebar.file_uploader(label='Upload your CSV or Excel file.',
                          type=['csv', 'xlsx'])
 
 
@@ -82,15 +82,15 @@ if uploaded_file:
     ######################################################
     st.sidebar.header('2. Display datasets')
 
-    check_box_raw_df = st.sidebar.checkbox(label="Display loaded dataset", value=True)
+    check_box = st.sidebar.checkbox(label="Display loaded dataset", value=True)
     #
-    if check_box_raw_df:
+    if check_box:
         st.write('### Loaded dataset:')
         st.write(df_raw)
 
-    check_box_clean_df = st.sidebar.checkbox(label="Display clean dataset")
+    check_box2 = st.sidebar.checkbox(label="Display clean dataset")
     #
-    if check_box_clean_df:
+    if check_box2:
         st.write("""
         ### Dataset after cleaning
         Dataset cleaning includes:
@@ -127,38 +127,37 @@ if uploaded_file:
         Blue colors depict negative, orange colors depict positive correlation.
         \n Include more attribute to the correlation matrix from the sidebar on the left handside.
         """)
-        check_box_corr_plot = st.sidebar.checkbox(label="Display plot of correlation matrix. (Consumes memory)")
+        check_box_corr_plot = st.sidebar.checkbox(label="Display plot of correlation matrix")
         #
         if check_box_corr_plot:
-          if len(corr_matrix.columns) < 50:
-            fig, ax = plt.subplots(figsize=(25, 20))
+            if len(corr_matrix.columns) < 50:
+                fig, ax = plt.subplots(figsize=(25, 20))
 
-            # create seaborn heatmap
-            g = sns.heatmap(corr_matrix, annot=True, linewidths=.5, center=0)
+                # create seaborn heatmap
+                g = sns.heatmap(corr_matrix, annot=True, linewidths=.5, center=0)
 
-            g.set_yticklabels(g.get_yticklabels(), rotation = 0, fontsize = 25)
-            g.set_xticklabels(g.get_xticklabels(), rotation = 90, fontsize = 25)
+                g.set_yticklabels(g.get_yticklabels(), rotation = 0, fontsize = 25)
+                g.set_xticklabels(g.get_xticklabels(), rotation = 90, fontsize = 25)
 
 
-            try:
-                g;
-                st.write(fig)
+                try:
+                    g;
+                    st.write(fig)
 
-            except Exception as e:
-                print(e)
-                st.write('Please select attributes to see correlation.')
-          else:
-            st.error("Your correlation matrix includes " + str(len(corr_matrix.columns)) + "criteria (selected columns X their unique valus). Max criteria level is set to 50 for visibility")
-
+                except Exception as e:
+                    print(e)
+                    st.write('Please select attributes to see correlation.')
+            else:
+                st.error("Your corr_matrix includes " + str(len(corr_matrix.columns)) + "criteria which is more than the accepted level: 50")
 
 
         ###############################################################################
 
 
         st.sidebar.header("4. Top correlations with sentences")
-        check_box_sentences = st.sidebar.checkbox(label="Show highly correlated attributes")
+        check_box3 = st.sidebar.checkbox(label="Show highly correlated attributes")
         #
-        if check_box_sentences:
+        if check_box3:
             treshold = st.sidebar.slider('Change correlation range to show highly correlated attributes.', 0.0, 1.0, 0.3)
             # treshold = treshold/10
             list_of_corr = []
